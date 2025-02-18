b955e8c17566eb9f9ced38865960a4c58bc4312d
KiHyeonYun <ykh094@naver.com>
2025-02-18 22:50:29 +0900

    1장 Object class 문제 풀어보기

---
+++ src/object/Rectangle.java
@@ -0,0 +1,25 @@
+package object;
+
+public class Rectangle {
+    private final int width;
+    private final int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Rectangle rectangle)) return false;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
